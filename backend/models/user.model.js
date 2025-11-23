8ab2daa6307d330e5461275b7b9e3ba830c5133e
jim amuto <jim.amuto@strathmore.edu>
2025-11-23 10:25:55 +0300

    moved files to a specialised folder

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Schema= new mongoose.Schema();
+const bcrypt=require('bcryptjs');
 
 const userschema=mongoose.Schema({
     name:{type:String,required:true},
@@ -12,6 +13,19 @@ const userschema=mongoose.Schema({
 },{
     timestamps:true
 })
+
+
+userschema.pre('save',async function(next){
+    if(!this.isModified('password'))next();
+    const salt=await bcrypt.genSalt(10);
+    this.password= await bcrypt.hash(this.password,salt);
+    next();
+});
+
+//custom method to match password
+userschema.methods.matchPassword=async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password);
+}
 
 const User=mongoose.model("User",userschema);
 module.exports=User;
